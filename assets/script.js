3409775ae1ba4ff5254e879c0f4944d25f5f0fb1
Trong Le <change1098@gmail.com>
2022-11-15 13:14:42 -0700

    v2.0 added time blocks and JS coding

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,23 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var dateEl = $('#currentDay');
+var curDate = dayjs()
+
+
 $(function () {
+dateEl.text(curDate)
+$( ".time-block" ).each(function( index,timeBlock ) {
+  var hour = $(timeBlock).children('div')
+  var textarea = $(timeBlock).children('textarea')
+  var button = $(timeBlock).children('button')
+//grab text from local storage
+let timeBlockTime = $(timeBlock).attr('id').split('-')[1]
+console.log(timeBlockTime)
+  button.on('click', function(event){
+var thisBtn = $(event.target)
+//save text to local storage
+
+  })
+});
+});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +36,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
